cd130ca17a0de8509357fc2c6c47a184f3e9dc2e
kibrom <kidanekibrom@gmail.com>
2024-05-27 09:13:24 +0300

    error middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+import productRoutes from "./routes/productRoutes.js"
 dotenv.config();
 const port = process.env.PORT || 5000;
 
@@ -9,17 +11,13 @@ connectDB();
 
 const app = express();
 
+app.use('/api/products', productRoutes);  
+
 app.get('/', (req, res) => {
     res.send('API is running');
 });
 
-app.get('/api/products', (req, res)=>{
-    res.json(products)
-});
-
-app.get('/api/products/:id', (req, res)=>{
-    const product = products.find((p)=>p._id===req.params.id);
-    res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, ()=> console.log(`Server running on port ${port}`));
